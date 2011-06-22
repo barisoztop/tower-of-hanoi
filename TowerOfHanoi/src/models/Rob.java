0348cbad710e2f30cf8ba4cfe03eb554d1dfa514
gokhanyilmaz1988 <gokhanyilmaz1988@gmail.com>
2011-06-22 23:57:27 +0000

    Rob Implementation

--- TowerOfHanoi/src/models/Rob.java
+++ TowerOfHanoi/src/models/Rob.java
@@ -5,13 +5,16 @@
 package models;
 
 import com.sun.j3d.utils.geometry.Cylinder;
+import java.awt.Color;
 import java.util.Stack;
 import javax.media.j3d.Appearance;
 import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Geometry;
+import javax.media.j3d.GeometryArray;
 import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
-import javax.vecmath.Vector3f;
+import javax.vecmath.Color3f;
+import javax.vecmath.Vector3d;
 
 /**
  *
@@ -28,25 +31,40 @@ public class Rob extends BranchGroup
     public Rob(float radius,float height,Appearance app) 
     {
         stack = new Stack<Disc>();
+        
         transGroup = new TransformGroup();
         transGroup.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
         transGroup.setCapability(TransformGroup.ALLOW_CHILDREN_EXTEND);
         transGroup.setCapability(TransformGroup.ALLOW_CHILDREN_READ);
         transGroup.setCapability(TransformGroup.ALLOW_CHILDREN_WRITE);
         
-        // Create your Shape
+        // Create Cylinder
+        
         cylinder = new Cylinder(radius, height, app);
         this.radius=radius;
         this.radius=height;
+        Transform3D tran = new Transform3D();
+        tran.rotX(Math.PI/2);
+        tran.setTranslation(new Vector3d(0.0,0.0,height/2));
+        TransformGroup cylinderGroup = new TransformGroup(tran);
+        cylinderGroup.addChild(cylinder);
         
-        //translation
+        //Create shadow
         Transform3D trans = new Transform3D();
-        trans.rotX(Math.PI/2.0);
-//        trans.setTranslation(new Vector3f(1.0f, 0.0f, height/2.0f));
-        transGroup.setTransform(trans);
+//        tran.rotX(Math.PI/2);
+        tran.setTranslation(new Vector3d(0.0,0.0,height/2));
+        TransformGroup shadowGroup = new TransformGroup(trans);
+        FakeShadow shadow = new FakeShadow((GeometryArray) getGeometry(), new Color3f(0.0f,0.0f,0.0f));
+        
+        shadowGroup.addChild(shadow);
+        
+        BranchGroup group = new BranchGroup();
+        group.addChild(cylinderGroup);
+//        group.addChild(shadowGroup);
         
         
-        transGroup.addChild(cylinder);
+        
+        transGroup.addChild(group);
         setCapability(ALLOW_CHILDREN_EXTEND);
         setCapability(ALLOW_CHILDREN_WRITE);
         setCapability(ALLOW_DETACH);
