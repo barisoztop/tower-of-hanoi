aeac3cdf820495f6c8e184d54d58e3296cf5588f
gokhanyilmaz1988@gmail.com <gokhanyilmaz1988@gmail.com@b64a4000-051b-a15a-d7f8-72cb9aa58d23>
2011-06-22 19:25:21 +0000

    first project commit for utilities and some required models like fakeshadow and blueAppearance

--- trunk/TowerOfHanoi/src/towerofhanoi/TowerOfHanoi.java
+++ trunk/TowerOfHanoi/src/towerofhanoi/TowerOfHanoi.java
@@ -11,10 +11,5 @@ package towerofhanoi;
 public class TowerOfHanoi 
 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+   
 }
