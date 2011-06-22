fa2d3210f0fac7db5da67e798cd9d88e3ec34061
gokhanyilmaz1988@gmail.com <gokhanyilmaz1988@gmail.com@b64a4000-051b-a15a-d7f8-72cb9aa58d23>
2011-06-22 21:14:02 +0000

    libraries and dataflow commit

--- trunk/TowerOfHanoi/src/models/Disc.java
+++ trunk/TowerOfHanoi/src/models/Disc.java
@@ -4,11 +4,28 @@
  */
 package models;
 
+import com.sun.j3d.utils.geometry.Cylinder;
+import javax.media.j3d.BranchGroup;
+import javax.media.j3d.TransformGroup;
+
 /**
  *
  * @author gokhan
  */
-public class Disc 
+public class Disc extends BranchGroup
 {
+
+    private float radius;
+    private float height;
+    
+    private final TransformGroup transGroup;
+    private final Cylinder cylinder;
+    
+    public Disc()
+    {
+        transGroup = new TransformGroup();
+        cylinder = new Cylinder();
+    }
+    
     
 }
