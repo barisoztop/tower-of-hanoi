59a92d53eabec9da27638bcb092d4e28d5a267d6
gokhanyilmaz1988 <gokhanyilmaz1988@gmail.com>
2011-06-22 19:24:14 +0000

    first project commit for utilities and some required models like fakeshadow and blueAppearance

---
+++ TowerOfHanoi/src/utils/BackgroundObject.java
@@ -0,0 +1,35 @@
+package utils;
+
+import java.awt.image.BufferedImage;
+
+import javax.media.j3d.Background;
+import javax.media.j3d.BoundingSphere;
+import javax.media.j3d.BranchGroup;
+import javax.media.j3d.ImageComponent2D;
+import javax.vecmath.Point3d;
+
+public class BackgroundObject extends BranchGroup {
+
+	private final Background background;
+	private BufferedImage bgImage;
+	
+	public BackgroundObject() {
+		background = new Background();
+		bgImage = new BufferedImage(ImageReceiver.X_PIXEL, ImageReceiver.Y_PIXEL, BufferedImage.TYPE_BYTE_GRAY);
+		ImageComponent2D ic = new ImageComponent2D(ImageComponent2D.FORMAT_CHANNEL8, bgImage, true, false);
+		background.setImage(ic);
+		background.setCapability(Background.ALLOW_IMAGE_WRITE);
+                
+                setCapability(ALLOW_CHILDREN_WRITE);
+                setCapability(ALLOW_DETACH);
+		background.setImageScaleMode(Background.SCALE_FIT_ALL);
+		background.setApplicationBounds(new BoundingSphere(new Point3d(), 100.0));
+		addChild(background);
+	}
+
+	public Background getBackground() {
+		return background;
+	}
+	
+	
+}
---
+++ TowerOfHanoi/src/utils/PoseReceiver.java
@@ -0,0 +1,61 @@
+package utils;
+
+import javax.media.j3d.Transform3D;
+import javax.media.j3d.TransformGroup;
+import javax.vecmath.Quat4d;
+import javax.vecmath.Vector3d;
+
+
+import ubitrack.SimplePose;
+import ubitrack.SimplePoseReceiver;
+
+public class PoseReceiver extends SimplePoseReceiver {
+
+	protected TransformGroup markerTransGroup = null;
+	
+    protected Vector3d transVec;
+    protected Quat4d rotQ;
+
+    public Quat4d getRotQ() {
+        return rotQ;
+    }
+
+    public Vector3d getTransVec() {
+        return transVec;
+    }
+        
+        
+    public void setTransformGroup(TransformGroup markerTransGroup) {
+		this.markerTransGroup = markerTransGroup;
+    }
+
+    public TransformGroup getMarkerTransGroup() {
+        return markerTransGroup;
+    }
+    
+	
+    @Override
+	public void receivePose(SimplePose pose) 
+        {
+            
+                
+            if (markerTransGroup == null)
+            {
+                    return;
+            }
+
+
+            transVec = new Vector3d(pose.getTx(), pose.getTy(), pose.getTz());
+            rotQ = new Quat4d(pose.getRx(), pose.getRy(), pose.getRz(), pose.getRw());
+
+            Transform3D markerTransform = new Transform3D();
+            markerTransform.set(rotQ, transVec, 1);
+            
+
+
+            markerTransGroup.setTransform(markerTransform);
+//            markerTransGroup.setCapability(TransformGroup.ALLOW_CHILDREN_WRITE);
+                
+	}
+    
+}
