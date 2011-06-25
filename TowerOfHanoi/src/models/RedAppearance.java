b755acc7803ca338261529e3ec466ff02a1bf225
gokhanyilmaz1988 <gokhanyilmaz1988@gmail.com>
2011-06-25 19:13:13 +0000

    arrow implementation

--- TowerOfHanoi/src/models/RedAppearance.java
+++ TowerOfHanoi/src/models/RedAppearance.java
@@ -1,6 +1,5 @@
 package models;
 
-import java.awt.Color;
 import javax.media.j3d.Appearance;
 import javax.media.j3d.Material;
 import javax.vecmath.Color3f;
@@ -11,8 +10,8 @@ public class RedAppearance extends Appearance{
 	public RedAppearance()
 	{
 		Color3f m_black = new Color3f(0.f,0.f,0.f);
-		Color3f m_white = new Color3f(1.0f,1.0f,1.0f);
-		Color3f m_red = new Color3f(Color.red);
+		Color3f m_white = new Color3f(0.5f,0.5f,0.5f);
+		Color3f m_red = new Color3f(0.8f,0.3f,0.3f);
 	
 		// Ambient,emissive,diffuse,specular,shininess
 		/*Material blueMat = new Material
